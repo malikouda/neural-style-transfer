e2f62817bc4930160569ff2694ae7a25d4d23f2b
Malik <malikouda@gmail.com>
2021-03-09 16:32:25 -0600

    fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="neural-style-transfer",
+    name="neural_style_transfer",
     version="0.0.1",
     description="Neural style transfer",
     packages=["neural_style_transfer"],
